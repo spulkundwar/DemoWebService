38d36a115f8d4b82e90c3d3e69b95e5d82e7bf10
spulkundwar <spulkundwar@gmail.com>
2022-04-02 08:44:37 +0100

    Initial commit

---
+++ POCs/DotNet6ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading;
+
+namespace DotNet6ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Program Execution started at {DateTime.Now}");
+            Random rm = new Random();
+            int seconds = rm.Next(2, 10);
+            Thread.Sleep(seconds * 1000);
+            Console.WriteLine($"Program Execution end at {DateTime.Now}");
+        }
+    }
+}
